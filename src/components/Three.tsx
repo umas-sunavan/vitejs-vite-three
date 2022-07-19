a1f5e35d9717cc940f054bfd2847985652b834cf
Umas Sunavan <fareastsunflower@gmail.com>
2022-07-19 10:22:36 +0800

    refactor code

--- src/components/Three.tsx
+++ src/components/Three.tsx
@@ -1,9 +1,9 @@
-import { Effects, OrbitControls, useGLTF } from "@react-three/drei";
-import { Canvas, extend, MeshBasicMaterialProps, Object3DNode, useFrame, Vector3 } from "@react-three/fiber";
+import { Effects, OrbitControls, OrbitControlsProps, useGLTF } from "@react-three/drei";
+import { Canvas, extend, MeshBasicMaterialProps, Object3DNode, useFrame, useThree, Vector3 } from "@react-three/fiber";
 import { MutableRefObject, useEffect, useMemo, useRef, useState } from "react";
-import { BoxGeometry, Color, GridHelper, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, Object3D, PlaneGeometry } from "three";
+import { BoxGeometry, BufferGeometry, Color, DoubleSide, GridHelper, Group, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, Object3D, PlaneGeometry, Vector3 as V3 } from "three";
 import { UnrealBloomPass } from "three-stdlib";
-import { useSpring, a } from '@react-spring/three'
+import { useSpring, a, SpringValue } from '@react-spring/three'
 
 
 // Create our custom element
@@ -21,52 +21,122 @@ declare global {
     }
 }
 
-export default function ThreeDom() {
-    // const [heightValues, setHeightValue] = useState(new Array(100).fill(null).map(() => Math.floor(Math.random() * 3 * 100) / 100))
-    // let maxHeightIndex = 0
-    // heightValues.forEach((value, i) => { if (value > heightValues[maxHeightIndex]) { maxHeightIndex = i } })
-    // const boxTable = new Array(10).fill(null).map((v, y) => {
-    //     return new Array(10).fill(null).map((v, x) => {
-    //         const height = heightValues[y * 10 + x]
-    //         return <Box key={x + '' + y} position={[1.5 * x - 5, 1.5 * y - 5, height / 2]} scale={[1, 1, height]} isHighest={height === heightValues[maxHeightIndex]} />
-    //     })
-    // })
+const CameraAnimation = (props: any) => {
+    const [started, setStarted] = useState(false)
+    const vec = new V3();
+  
+    useEffect(() => {
+      setStarted(true);
+    });
+  
+    useFrame(state => {
+        const position:V3 = props.pos ? props.pos : [10, -5, 5]
+        const distance = state.camera.position.distanceTo(position)
+        if (distance > 0.8) {
+            if (started) {
+              state.camera.lookAt(10,5,0);
+              state.camera.position.lerp(vec.set(position.x, state.camera.position.y, position.z + 10), .08)
+            } 
 
-    const [floorsValues, setFloorsValue] = useState(new Array(100).fill(null).map(() => Math.floor(Math.random() * 30)))
-    let maxFloorIndex = 0
-    const box = new Mesh(new BoxGeometry(10, 10, 10), new MeshPhongMaterial())
-    box.translateZ
-    floorsValues.forEach((value, i) => { if (value > floorsValues[maxFloorIndex]) { maxFloorIndex = i } })
+        }
+        return null
+        
+    })
+     return null;
+  }
+
+  function BoxScene () {
+      const [heightValues, setHeightValue] = useState(new Array(100).fill(null).map(() => Math.floor(Math.random() * 3 * 100) / 100))
+      let maxHeightIndex = 0
+      heightValues.forEach((value, i) => { if (value > heightValues[maxHeightIndex]) { maxHeightIndex = i } })
+      const boxTable = new Array(10).fill(null).map((v, y) => {
+          return new Array(10).fill(null).map((v, x) => {
+              const height = heightValues[y * 10 + x]
+              return <Box key={x + '' + y} position={[1.5 * x - 5, 1.5 * y - 5, height / 2]} scale={[1, 1, height]} isHighest={height === heightValues[maxHeightIndex]} />
+          })
+      })
+      return (
+          <Canvas camera={{ fov: 45, position:  [10, -5, 5]}} style={{ height: '100vh', width: '100vw' }}>
+              <CameraAnimation />
+              <OrbitControls/>
+              <ambientLight intensity={0.5} />
+              <fog attach="fog" args={['#B8860B', 5, 20]} />
+              <Effects disableGamma>
+                  <customElement threshold={0.4} strength={0.3} radius={0.6} />
+              </Effects>
+              <pointLight position={[10, 10, 10]} intensity={1} />
+              {boxTable}
+              <mesh scale={[1, 1, 1]} >
+                  <planeGeometry args={[40, 40, 40]}></planeGeometry>
+                  <meshPhongMaterial side={DoubleSide} color={'#666644'}></meshPhongMaterial>
+              </mesh>
+          </Canvas>
+      )
+  }
+
+  function BuildingScene () {
+    
+    const _getHighest = () => {
+        let highestFloorId = 0
+        buildingFloors.forEach((value, i) => { if (value > buildingFloors[highestFloorId]) { highestFloorId = i } })
+        return highestFloorId
+    }
+    const [buildingFloors, setFloorsValue] = useState(new Array(100).fill(null).map(() => Math.floor(Math.random() * 30)))
+    const [buildingClicked, setCanvasClicked] = useState(undefined) as [MutableRefObject<Group> | undefined, any]
+    const highestBuildingId = _getHighest()
     const buildingTable = new Array(10).fill(null).map((v, y) => {
         return new Array(10).fill(null).map((v, x) => {
-            const height = floorsValues[y * 10 + x]
-            return <Model key={x + '' + y} position={[2 * x - 0, 2 * y - 0, 0]} floorCount={height} isHighest={height === floorsValues[maxFloorIndex]} />
+            const height = buildingFloors[y * 10 + x]
+            const modelId = +(y * 10 + x)
+            return <Model 
+            key={modelId}
+            position={[2 * x - 0, 2 * y - 0, 0]} 
+            floorCount={height} 
+            isHighest={height === buildingFloors[highestBuildingId]} 
+            onPointerOver={(ref:MutableRefObject<Group>)=> {
+                setCanvasClicked(ref)
+            }}/>
         })
     })
-    const [isCanvasClicked, setCanvasClicked] = useState(false)
-    const props = useSpring({
-        position: isCanvasClicked ? [10, 10, 0] : [0, 0, 0]
+    const getPosition = (building:MutableRefObject<Group> | undefined) => {
+        return building ? building.current.position.toArray() : [0,0,0]
+    }
+    const animationProps = useSpring({
+        position: getPosition(buildingClicked)
     })
-
-    return (
-        <Canvas style={{ height: '100vh', width: '100vw' }} onClick={() => setCanvasClicked(!isCanvasClicked)}>
-            <OrbitControls makeDefault />
+    const orbitRef = useRef<MutableRefObject<OrbitControlsProps>>();
+    const enableCameraLooking = false
+    useEffect( () => {
+        // if (!orbitRef.current) return 
+        // console.log((orbitRef.current as any).target);
+        // if (!(orbitRef.current as any).target) return 
+        // (orbitRef.current as any).target = new V3(5,5,5);
+        // console.log((orbitRef.current as any).target);
+        // (orbitRef.current as any).update()
+    })
+      return (
+        <Canvas camera={{ fov: 45, position:  [10, -5, 5]}} style={{ height: '100vh', width: '100vw' }}>
+            {enableCameraLooking ? <CameraAnimation pos={buildingClicked?.current.position}/> : ''}
+            <OrbitControls ref={orbitRef} target={enableCameraLooking ? buildingClicked?.current.position.toArray() : [10,5,0]}/>
             <ambientLight intensity={0.5} />
             <fog attach="fog" args={['#B8860B', 5, 20]} />
             <Effects disableGamma>
-                {/* <unrealBloomPass threshold={1} strength={1.0} radius={0.5} /> */}
                 <customElement threshold={0.4} strength={0.3} radius={0.6} />
             </Effects>
             <pointLight position={[10, 10, 10]} intensity={1} />
-            {/* {boxTable} */}
             {buildingTable}
-            <a.mesh scale={[1, 1, 1]} position={props.position} >
+            <a.mesh scale={[1, 1, 1]} position={animationProps.position} >
                 <planeGeometry args={[40, 40, 40]}></planeGeometry>
-                <meshPhongMaterial color={'#666644'}></meshPhongMaterial>
+                <meshPhongMaterial side={DoubleSide} color={'#666644'}></meshPhongMaterial>
             </a.mesh>
-            {/* <Model position={[1 - 0, 0 - 0, 0]} floorCount={12} />
-            <Model position={[2 - 0, 0 - 0, 0]} floorCount={12} /> */}
         </Canvas>
+      )
+  }
+
+export default function ThreeDom() {
+    return (
+        // <BoxScene></BoxScene>
+        <BuildingScene></BuildingScene>
     )
 }
 
@@ -142,7 +212,10 @@ function Model(props: any) {
     })
     return (<primitive {...props}
         onClick={() => click(!clicked)}
-        onPointerOver={() => hover(true)}
+        onPointerOver={() => {
+            props.onPointerOver(ref)
+            hover(true)}
+        }
         onPointerOut={() => hover(false)}
         object={scene}
         scale={0.0005}
